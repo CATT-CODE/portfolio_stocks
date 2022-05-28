16a6fc98262e0c6fa9e231e3eb05c81c29d00aa3
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2022-05-28 10:56:41 -0400

    Initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static"),
 # )
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- stocksapp/urls.py
+++ stocksapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+
 app_name = 'stocksapp'
 urlpatterns = [
 	path('', views.charts, name='charts'),
