49c9ef7126ff505acd049134ced1d3dabc04fcea
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2022-05-28 05:47:47 -0400

    Initial commit

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -5,7 +5,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 import os
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 application = get_wsgi_application()
