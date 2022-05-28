60bc7d4767dd0d9de6b0b3d770810c5c04a0f26c
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2022-05-28 05:40:30 -0400

    Initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,43 +21,44 @@ ALLOWED_HOSTS = ['stockmatrix.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+		'django.contrib.admin',
+		'django.contrib.auth',
+		'django.contrib.contenttypes',
+		'django.contrib.sessions',
+		'django.contrib.messages',
+		"whitenoise.runserver_nostatic",
+		'django.contrib.staticfiles',
 		'stocksapp',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+		'django.middleware.security.SecurityMiddleware',
 		'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+		'django.contrib.sessions.middleware.SessionMiddleware',
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.contrib.auth.middleware.AuthenticationMiddleware',
+		'django.contrib.messages.middleware.MessageMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+		{
+				'BACKEND': 'django.template.backends.django.DjangoTemplates',
+				'DIRS': [os.path.join(BASE_DIR, 'templates')],
+				'APP_DIRS': True,
+				'OPTIONS': {
+						'context_processors': [
+								'django.template.context_processors.debug',
+								'django.template.context_processors.request',
+								'django.contrib.auth.context_processors.auth',
+								'django.contrib.messages.context_processors.messages',
+						],
+				},
+		},
 ]
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
@@ -67,10 +68,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+		'default': {
+				'ENGINE': 'django.db.backends.sqlite3',
+				'NAME': BASE_DIR / 'db.sqlite3',
+		}
 }
 
 
@@ -78,18 +79,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+		{
+				'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+		},
+		{
+				'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+		},
+		{
+				'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+		},
+		{
+				'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+		},
 ]
 
 
@@ -109,7 +110,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+		os.path.join(BASE_DIR, "static")
 ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
