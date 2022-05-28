d699a05bfd7f63896ba6b171a47e3e456f8186aa
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2022-05-28 19:40:48 -0400

    Initial commit

--- stocksapp/views.py
+++ stocksapp/views.py
@@ -18,7 +18,7 @@ now = timezone.now()
 
 def charts(request):
 	
-	def newTicker(search=request.session['meta_data'] if 'meta_data' in request.session else random.choices(['GME', 'AAPL', 'AMZN', 'TSLA'])[0], start=now.date() - timedelta(days = 365), end=now.date()):
+	def newTicker(search=request.session['meta_data'] if 'meta_data' in request.session else 'GME', start=now.date() - timedelta(days = 810), end=now.date()):
 		search = search.upper()
 		tickerInfo = yf.Ticker(search).info
 		try: 
